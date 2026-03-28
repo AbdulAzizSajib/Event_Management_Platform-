3bec6de8bc8dfaf5bf745020b107258215fb4509
Abdul Aziz Sajib <abdulazizsajib@gmail.com>
2026-03-28 18:52:47 +0600

    Google Login Issue for Deployment

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -7,10 +7,7 @@ import { Role, UserStatus } from "../../generated/prisma/enums";
 import { sendEmail } from "../utils/email";
 
 export const auth = betterAuth({
-  // baseURL: envVars.BETTER_AUTH_URL,
-
-  // for deployment
-  baseURL: process.env.FRONTEND_URL,
+  baseURL: envVars.BETTER_AUTH_URL,
 
   secret: envVars.BETTER_AUTH_SECRET,
   database: prismaAdapter(prisma, {
@@ -135,13 +132,10 @@ export const auth = betterAuth({
     signIn: `${envVars.BETTER_AUTH_URL}/api/v1/auth/google/success`,
   },
 
-  // trustedOrigins: [
-  //   process.env.BETTER_AUTH_URL || "http://localhost:5000",
-  //   envVars.FRONTEND_URL,
-  // ],
-
-  //  for diployment
-  trustedOrigins: [process.env.FRONTEND_URL!],
+  trustedOrigins: [
+    envVars.BETTER_AUTH_URL,
+    envVars.FRONTEND_URL,
+  ],
 
   session: {
     expiresIn: 60 * 60 * 60 * 24, // 1 day in seconds
